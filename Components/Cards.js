03e2cb05e743286ff60415174925256f89ef85a6
sonukuldeep <sonukuldeep@outlook.com>
2022-12-15 13:59:07 +0530

    some ui changes and db update

--- Components/Cards.js
+++ Components/Cards.js
@@ -1,18 +1,19 @@
 import style from '../styles/Cards.module.scss'
+import Link from 'next/link'
 
 const Cards = ({ cardsToDisplay, showRank = false, smallSquare = false, order = 0 }) => {
 
   return (
     <div className={smallSquare ? `${style.bigContainer_reverse}` : `${style.bigContainer}`}>
       {order === 1 ? <div className={style.plankCard}>
-        <p>Order custom pc for any use case at an affordable price</p>
-        <p>Have a broken laptop or pc? </p>
-        <p>Or you want to sell your old computer?</p>
-        <p>Call us or whatsApp us and we will take care of the rest</p>
-        <p>Whatsapp number: 8270333710</p>
+          <p>Order custom pc for any use case at an affordable price</p>
+          <p>Have a broken laptop or pc? </p>
+          <p>Or you want to sell your old computer?</p>
+          <p>Call us or whatsApp us and we will take care of the rest</p>
+          <p>Whatsapp number: 8270333710</p>
       </div> : ""}
       <div className={!smallSquare ? `${style.container}` : `${style.smallContainer}`}>
-        {cardsToDisplay.map((card, index) => <div className={style.card} key={index}>{showRank ? <span>#{index + 1}</span> : ""}<img src={card.img} alt="card" /><span>{card.excerpt}</span></div>)}
+        {cardsToDisplay.map((card, index) => <div className={style.card} key={index}>{showRank ? <span>#{index + 1}</span> : ""}<Link href={`/products/${card.id}`}><img src={card.img} alt="card" /></Link><span>{card.excerpt}</span></div>)}
       </div>
       {order === 2 ? <div className={style.plankCard}>
         <p>Online delivery within 30min</p>
